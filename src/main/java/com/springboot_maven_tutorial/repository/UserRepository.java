02a45bf2f3862b872dafbcedba9e99727f31ccfe
Nikola <nikanberko286@gmail.com>
2022-10-21 15:42:13 +0200

    defined user repository and user model

--- src/main/java/com/springboot_maven_tutorial/repository/UserRepository.java
+++ src/main/java/com/springboot_maven_tutorial/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.springboot_maven_tutorial.repository;
 
-public interface UserRepository {
+
+import com.springboot_maven_tutorial.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
