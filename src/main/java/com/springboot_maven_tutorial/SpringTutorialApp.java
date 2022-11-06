b8cc96eb2881f139158d8c7436d6b13f6dcd12f9
Nikola <nikanberko286@gmail.com>
2022-11-06 18:34:52 +0100

    added main method

---
+++ src/main/java/com/springboot_maven_tutorial/SpringTutorialApp.java
@@ -0,0 +1,12 @@
+package com.springboot_maven_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorialApp.class, args);
+    }
+}
