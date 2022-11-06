9775f24bbf88d0573825d6704b1a8cd68d7b5bb0
Nikola <nikanberko286@gmail.com>
2022-11-06 18:32:00 +0100

    Defined openAPI configuration for swagger

--- src/main/java/com/springboot_maven_tutorial/config/OpenApiConfiguration.java
+++ src/main/java/com/springboot_maven_tutorial/config/OpenApiConfiguration.java
@@ -1,4 +1,33 @@
 package com.springboot_maven_tutorial.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("User Management")
+                                .description("User CRUD")
+                                .version("1.0")
+                                .license(
+                                        new License()
+                                                .name("Apache 2.0")
+                                                .url("http://www.apache.org/licenses/LICENSE-2.0")));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("User Management")
+                .pathsToMatch("/api/user/**")
+                .build();
+    }
 }
